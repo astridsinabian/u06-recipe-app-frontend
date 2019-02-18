fc4ea80285e5d74e4603d6eb3a7c0577018f6f6f
Astrid <astrid.sinabian@chasacademy.se>
2019-02-18 13:32:12 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
-
+import { LoginComponent } from './users/login/login.component';
+import { RegisterComponent } from './users/register/register.component';
+import { ProfileComponent } from './users/profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'recipes', component: RecipeListComponent },
-  { path: 'recipes/:id', component: RecipesDetailsComponent }
+  { path: 'recipes/:id', component: RecipesDetailsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
